ed0fc601821874eb9b7b694a7b14540ce1b825f2
andrem91 <andrem166@gmail.com>
2021-07-30 16:34:46 -0300

    first commit

---
+++ src/main/java/com/fullgames/lojagames/repository/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.fullgames.lojagames.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fullgames.lojagames.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+}
